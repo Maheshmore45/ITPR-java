fd1e762c44b629d24442e3cd195c23045d38a64b
Maheshmore45 <moremahesh8360@gmail.com>
2025-06-26 12:17:26 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,12 @@
+public class string {
+    public static void main(String[] args) {
+        
+        String  s = "abc";
+        String  a = "pqq";
+            s=a+s;
+            System.out.println(s);
+
+
+    }
+    
+}
